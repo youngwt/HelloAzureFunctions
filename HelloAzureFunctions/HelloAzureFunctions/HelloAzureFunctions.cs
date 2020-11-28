0b23d50d0b17db43911163376130e892596a069c
Wayne Young <youngwt@gmail.com>
2020-11-28 22:32:22 +0000

    changed to post only and edited the log message

--- HelloAzureFunctions/HelloAzureFunctions/HelloAzureFunctions.cs
+++ HelloAzureFunctions/HelloAzureFunctions/HelloAzureFunctions.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -14,20 +13,16 @@ namespace HelloAzureFunctions
     {
         [FunctionName("HelloAzureFunctions")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
+            log.LogInformation("My first Azure functions end point has been called!");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = 
+                "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
             return new OkObjectResult(responseMessage);
         }
